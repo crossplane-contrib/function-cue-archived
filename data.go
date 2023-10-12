97200a1859b7917daec2f44a153dd104bea719d3
Bryan Nobuhara <bnobuhara@gmail.com>
2023-10-12 17:06:47 -0400

    remove gatherpaths

--- data.go
+++ data.go
@@ -8,25 +8,6 @@ import (
 	"github.com/pkg/errors"
 )
 
-// gatherPaths recursively traverses the JSON data and collect jsonpaths in a set
-func gatherPaths(data interface{}, path string, paths map[string]struct{}) {
-	switch val := data.(type) {
-	case map[string]interface{}:
-		for key, value := range val {
-			newKey := fmt.Sprintf("%s.%v", path, key)
-			gatherPaths(value, newKey, paths)
-		}
-	case []interface{}:
-		for i, value := range val {
-			newPath := fmt.Sprintf("%s[%d]", path, i)
-			gatherPaths(value, newPath, paths)
-		}
-	default:
-		// Reached a leaf node, add the JSON path to the set
-		paths[path] = struct{}{}
-	}
-}
-
 // setData recursively traverses the JSON data and set the data into the DesiredComposed resource
 func setData(data interface{}, path string, d *resource.DesiredComposed) error {
 	switch val := data.(type) {
