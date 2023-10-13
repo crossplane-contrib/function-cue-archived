6cf37985905636a76fa7933db661df28a5983f06
Bryan Nobuhara <bnobuhara@gmail.com>
2023-10-13 12:01:50 -0400

    Rename input to CUEInput

--- fn.go
+++ fn.go
@@ -31,7 +31,7 @@ func (f *Function) RunFunction(_ context.Context, req *fnv1beta1.RunFunctionRequ
 
 	rsp := response.To(req, response.DefaultTTL)
 
-	in := &v1beta1.Input{}
+	in := &v1beta1.CUEInput{}
 	if err := request.GetInput(req, in); err != nil {
 		response.Fatal(rsp, errors.Wrapf(err, "cannot get function input from %T", req))
 		return rsp, nil
